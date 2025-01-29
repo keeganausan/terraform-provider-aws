dad162597857898f0aab365d101c245facef6e91
Keegan McIver <keegan.mciver@usanad.com>
2025-01-29 15:01:59 -0500

    update aws_connect_associate_phone_number_contact_flow ress=ource with proper importer

--- internal/service/connect/associate_phone_number_contact_flow.go
+++ internal/service/connect/associate_phone_number_contact_flow.go
@@ -8,18 +8,17 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/connect"
-	// awstypes "github.com/aws/aws-sdk-go-v2/service/connect/types"
+	awstypes "github.com/aws/aws-sdk-go-v2/service/connect/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	// "github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
-	// "github.com/hashicorp/terraform-provider-aws/internal/errs"
-	"github.com/hashicorp/terraform-provider-aws/internal/errs/sdkdiag"
+	"github.com/hashicorp/terraform-provider-aws/internal/errs"
 	"github.com/hashicorp/terraform-provider-aws/internal/tfresource"
 )
 
 const (
-	phoneNumberContactFlowResourceIDSeparator = ":"
+	phoneNumberContactFlowResourceIDSeparator = ","
 )
 // @SDKResource("aws_connect_associate_phone_number_contact_flow", name="Associate Phone Number Contact Flow")
 func resourceAssociatePhoneNumberContactFlow() *schema.Resource {
@@ -28,21 +27,21 @@ func resourceAssociatePhoneNumberContactFlow() *schema.Resource {
 		ReadWithoutTimeout:   resourceAssociatePhoneNumberContactFlowRead,
 		DeleteWithoutTimeout: resourceAssociatePhoneNumberContactFlowDelete,
 		Importer: &schema.ResourceImporter{
-			StateContext: resourcePhoneNumberContactFlowImport,
+			StateContext: schema.ImportStatePassthroughContext,
 		},
 
 		Schema: map[string]*schema.Schema{
-			"instance_id": {
+			"instance_arn": {
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
 			},
-			"phone_number_id": {
+			"phone_number_arn": {
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
 			},
-			"contact_flow_id": {
+			"contact_flow_arn": {
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
@@ -55,14 +54,14 @@ func resourceAssociatePhoneNumberContactFlowCreate(ctx context.Context, d *schem
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).ConnectClient(ctx)
 
-	instanceID := d.Get("instance_id").(string)
-	phoneNumberID := d.Get("phone_number_id").(string)
-	contactFlowID := d.Get("contact_flow_id").(string)
+	instanceARN := d.Get("instance_arn").(string)
+	phoneNumberARN := d.Get("phone_number_arn").(string)
+	contactFlowARN := d.Get("contact_flow_arn").(string)
 
 	input := &connect.AssociatePhoneNumberContactFlowInput{
-		InstanceId:     aws.String(instanceID),
-		PhoneNumberId:  aws.String(phoneNumberID),
-		ContactFlowId:  aws.String(contactFlowID),
+		InstanceId:     aws.String(instanceARN),
+		PhoneNumberId:  aws.String(phoneNumberARN),
+		ContactFlowId:  aws.String(contactFlowARN),
 	}
 
 	_, err := conn.AssociatePhoneNumberContactFlow(ctx, input)
@@ -70,7 +69,7 @@ func resourceAssociatePhoneNumberContactFlowCreate(ctx context.Context, d *schem
 		return diag.FromErr(err)
 	}
 
-	id := phoneNumberContactFlowCreateResourceID(instanceID, phoneNumberID, contactFlowID)
+	id := phoneNumberContactFlowCreateResourceID(instanceARN, phoneNumberARN)
 	d.SetId(id)
 
 	return append(diags, resourceAssociatePhoneNumberContactFlowRead(ctx, d, meta)...)
@@ -78,15 +77,11 @@ func resourceAssociatePhoneNumberContactFlowCreate(ctx context.Context, d *schem
 
 func resourceAssociatePhoneNumberContactFlowRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
+	conn := meta.(*conns.AWSClient).ConnectClient(ctx)
 
-	// instanceID := d.Get("instance_id").(string)
-	// phoneNumberID := d.Get("phone_number_id").(string)
-	// contact_flow_id := d.Get("contact_flow_id").(string)
+	instanceARN, phoneNumberARN, err := phoneNumberContactFlowParseResourceID(d.Id())
 
-	instanceID, phoneNumberID, contactFlowID, err := phoneNumberContactFlowParseResourceID(d.Id())
-	if err != nil {
-		return sdkdiag.AppendFromErr(diags, err)
-	}
+  contactFlowARN, err := findAssociatedContactFlow(ctx, conn, instanceARN, phoneNumberARN)
 
 	if !d.IsNewResource() && tfresource.NotFound(err) {
 		log.Printf("[WARN] Connect Instance Storage Config (%s) not found, removing from state", d.Id())
@@ -94,23 +89,56 @@ func resourceAssociatePhoneNumberContactFlowRead(ctx context.Context, d *schema.
 		return diags
 	}
 
-	d.Set("instance_id", instanceID)
-	d.Set("phone_number_id", phoneNumberID)
-	d.Set("contact_flow_id", contactFlowID)
+	d.Set("instance_arn", instanceARN)
+	d.Set("phone_number_arn", phoneNumberARN)
+	d.Set("contact_flow_arn", contactFlowARN)
 
 	return diags
+}
+
+func findAssociatedContactFlow(ctx context.Context, conn *connect.Client, instanceARN string, phoneNumberARN string) (string, error) {
+	input := &connect.ListFlowAssociationsInput{
+		InstanceId: aws.String(instanceARN),
+		ResourceType: awstypes.ListFlowAssociationResourceTypeVoicePhoneNumber,
+	}
+
+	output, err := conn.ListFlowAssociations(ctx, input)
+
+	if errs.IsA[*awstypes.ResourceNotFoundException](err) {
+		return "", &retry.NotFoundError{
+			LastError:   err,
+			LastRequest: input,
+		}
+	}
+
+	if err != nil {
+		return "", err
+	}
+
+	if output == nil || output.FlowAssociationSummaryList == nil {
+		return "", tfresource.NewEmptyResultError(input)
+	}
+
+  var flowAssociation string
+	for _, v := range output.FlowAssociationSummaryList {
+		if *v.ResourceId == phoneNumberARN {
+			flowAssociation = *v.FlowId
+		}
+	}
+
+	return flowAssociation, nil
 }
 
 func resourceAssociatePhoneNumberContactFlowDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
 	conn := meta.(*conns.AWSClient).ConnectClient(ctx)
 
-	instanceID := d.Get("instance_id").(string)
-	phoneNumberID := d.Get("phone_number_id").(string)
+	instanceARN := d.Get("instance_arn").(string)
+	phoneNumberARN := d.Get("phone_number_arn").(string)
 
 	input := &connect.DisassociatePhoneNumberContactFlowInput{
-		InstanceId:    aws.String(instanceID),
-		PhoneNumberId: aws.String(phoneNumberID),
+		InstanceId:    aws.String(instanceARN),
+		PhoneNumberId: aws.String(phoneNumberARN),
 	}
 
 	_, err := conn.DisassociatePhoneNumberContactFlow(ctx, input)
@@ -123,33 +151,18 @@ func resourceAssociatePhoneNumberContactFlowDelete(ctx context.Context, d *schem
 	return diags
 }
 
-func resourcePhoneNumberContactFlowImport(ctx context.Context, d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
-	parts := strings.SplitN(d.Id(), phoneNumberContactFlowResourceIDSeparator, 3)
-	if len(parts) != 3 || parts[0] == "" || parts[1] == "" || parts[2] == "" {
-		return nil, fmt.Errorf("unexpected format of ID (%[1]s), expected instanceID%[2]sphoneNumberID%[2]scontactFlowID", parts, phoneNumberContactFlowResourceIDSeparator)
-	}
-	instanceID := parts[0]
-	phoneNumberID := parts[1]
-	contactFlowID := parts[2]
-	d.Set("instance_id", instanceID)
-	d.Set("phone_number_id", phoneNumberID)
-	d.Set("contact_flow_id", contactFlowID)
-	d.SetId(fmt.Sprintf("%s:%s:%s", instanceID, phoneNumberID, contactFlowID))
-	return []*schema.ResourceData{d}, nil
-}
-
-func phoneNumberContactFlowCreateResourceID(instanceID, phoneNumberID string, contactFlowID string) string {
-	parts := []string{instanceID, phoneNumberID, contactFlowID}
+func phoneNumberContactFlowCreateResourceID(instanceARN, phoneNumberARN string) string {
+	parts := []string{instanceARN, phoneNumberARN}
 	id := strings.Join(parts, phoneNumberContactFlowResourceIDSeparator)
 
 	return id
 }
 
-func phoneNumberContactFlowParseResourceID(id string) (string, string, string, error) {
+func phoneNumberContactFlowParseResourceID(id string) (string, string, error) {
 	parts := strings.SplitN(id, phoneNumberContactFlowResourceIDSeparator, 3)
-	if len(parts) != 3 || parts[0] == "" || parts[1] == "" || parts[2] == "" {
-		return "", "", "", fmt.Errorf("unexpected format of ID (%[1]s), expected instanceID%[2]sphoneNumberID%[2]scontactFlowID", id, phoneNumberContactFlowResourceIDSeparator)
+	if len(parts) != 2 || parts[0] == "" || parts[1] == "" {
+		return "", "", fmt.Errorf("unexpected format of ID (%[1]s), expected instanceARN%[2]sphoneNumberARN", id, phoneNumberContactFlowResourceIDSeparator)
 	}
 
-	return parts[0], parts[1], parts[2], nil
+	return parts[0], parts[1], nil
 }
